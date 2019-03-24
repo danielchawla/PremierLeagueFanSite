740122e845d0bc8dfcaf88b659159fd1682fe7f6
Daniel Chawla <danielchawla93@gmail.com>
2019-03-24 01:26:57 -0700

    updated views

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,14 @@
 t1 = Team.create(shortname: 'ARS', fullname: 'Arsenal', yearfounded: 1886, budget: 100000000)
 t2 = Team.create(shortname: 'EVE', fullname: 'Everton', yearfounded: 1878, budget: 105060200)
 
-p1 = t1.players.create(firstname: 'Petr', lastname: 'Cech', nationality: 'Czech Republic', position: 'Goalkeeper', yearsofexperience: '4', salary: '100000') 
+t1.players.create(firstname: 'Petr', lastname: 'Cech', nationality: 'Czech Republic', position: 'Goalkeeper', yearsofexperience: '4', salary: '100000') 
+t2.players.create(firstname: 'Bob', lastname: 'Fren', nationality: 'Germany', position: 'Forward', yearsofexperience: '12', salary: '120322') 
+
+t1.coaches.create(firstname: 'Alice', lastname: 'Champ') 
+t2.coaches.create(firstname: 'Kim', lastname: 'King')
+
+t1.owners.create(firstname: 'Taksin', lastname: 'Mann') 
+t2.owners.create(firstname: 'Jane', lastname: 'Smith') 
+
+t1.managers.create(firstname: 'Jonathan', lastname: 'Crosby') 
+t2.managers.create(firstname: 'Fred', lastname: 'Williams') 
